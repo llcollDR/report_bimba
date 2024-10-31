2f925b216476aedf34e541ce9e58c8089c1d745f
Lluis Coll <llcoll@digitalresponse.es>
2024-10-31 10:56:54 +0100

    Testing function added

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-import pandas as pd
+from flask import Flask, render_template
+import cleaning
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+def testing_function():
+    print("Estamos testeando 1,2,3")
+
+if __name__ == "__main__":
+    app.run()
